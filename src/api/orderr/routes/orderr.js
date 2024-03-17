a0c7ec8035be7d1d41a745271ed6425b865664fb
marwan.hassan <mixing.gamer546@gmail.com>
2024-03-18 00:41:59 +0200

    Strapi Back-End

---
+++ src/api/orderr/routes/orderr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orderr router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::orderr.orderr');
---
+++ src/api/orderr/services/orderr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orderr service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::orderr.orderr');
